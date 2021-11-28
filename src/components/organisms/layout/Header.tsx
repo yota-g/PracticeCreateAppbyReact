0422b61f4af7569cb13827ea76d51ae4366a0835
yota-g <64677279+yota-g@users.noreply.github.com>
2021-11-29 00:12:37 +0900

    separate component header

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,19 +1,9 @@
-import {
-  Flex,
-  Heading,
-  Link,
-  Box,
-  Drawer,
-  DrawerOverlay,
-  DrawerContent,
-  DrawerBody,
-  Button,
-  useDisclosure
-} from "@chakra-ui/react";
+import { Flex, Heading, Link, Box, useDisclosure } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 // import { HamburgerIcon } from "@chakra-ui/icons";
 
 import { MenuIconButton } from "../../atom/button/MenuIconButton";
+import { MenuDrawer } from "../../molecules/MenuDrawer";
 
 export const Header: VFC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -49,17 +39,7 @@ export const Header: VFC = memo(() => {
         </Flex>
         <MenuIconButton onOpen={onOpen} />
       </Flex>
-      <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
-        <DrawerOverlay>
-          <DrawerContent>
-            <DrawerBody p={0} bg="gray.100">
-              <Button w="100%">TOP</Button>
-              <Button w="100%">ユーザー一覧</Button>
-              <Button w="100%">設定</Button>
-            </DrawerBody>
-          </DrawerContent>
-        </DrawerOverlay>
-      </Drawer>
+      <MenuDrawer isOpen={isOpen} onClose={onClose} />
     </>
   );
 });
