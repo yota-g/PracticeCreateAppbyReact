43fe89743a27e3b620ee156587d3c871f2b986b0
yota-g <64677279+yota-g@users.noreply.github.com>
2021-11-30 22:10:21 +0900

    create users list getting function

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,18 +1,34 @@
-import { Wrap, WrapItem } from "@chakra-ui/react";
-import { memo, VFC } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+import { memo, useEffect, VFC } from "react";
+
 import { UserCard } from "../organisms/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: VFC = memo(() => {
+  const { getUsers, users, loading } = useAllUsers();
+  useEffect(() => getUsers(), []);
+  //初期配列にしておくと、画面表示時に一覧が表示される
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      {/* 携帯の時はpが小さくて大きな画面の時は、大きくなるようになる。　 */}
-      <WrapItem>
-        <UserCard
-          imageUrl="https://source.unsplash.com/random"
-          userName="じゃけぇ"
-          fullName="Takumi Okada"
-        />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {/* 携帯の時はpが小さくて大きな画面の時は、大きくなるようになる。　 */}
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   );
 });
