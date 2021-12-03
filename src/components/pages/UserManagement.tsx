4b8c311f9c45300d1e1502e52cd2b6b9a8fc7d92
yota-g <64677279+yota-g@users.noreply.github.com>
2021-12-03 22:06:15 +0900

    create login user info context

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -12,11 +12,14 @@ import { UserCard } from "../organisms/user/UserCard";
 import { useAllUsers } from "../../hooks/useAllUsers";
 import { UserDetailModal } from "../organisms/user/UserDetailModal";
 import { useSelectUser } from "../../hooks/useSelectUser";
+import { useLoginUser } from "../../hooks/useLoginUser";
 
 export const UserManagement: VFC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const { getUsers, users, loading } = useAllUsers();
   const { onSelectUser, selectedUser } = useSelectUser();
+  const { loginUser } = useLoginUser();
+  // console.log(loginUser);
   // console.log(selectedUser);
   useEffect(() => getUsers(), []);
   //propsで渡す関数は毎回レンダリングされると困るのでuseCallbackを使用する。
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -4,11 +4,13 @@ import { useHistory } from "react-router-dom";
 
 import { User } from "../types/api/user";
 import { useMessage } from "./useMessage";
+import { useLoginUser } from "../hooks/useLoginUser";
 
 export const useAuth = () => {
   const history = useHistory();
   const { showMessage } = useMessage();
   const [loading, setLoading] = useState(false);
+  const { setLoginUser } = useLoginUser();
 
   const login = useCallback(
     (id: string) => {
@@ -17,18 +19,22 @@ export const useAuth = () => {
         .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then((res) => {
           if (res.data) {
+            setLoginUser(res.data);
             showMessage({ title: "ログインしました", status: "success" });
             history.push("/home");
           } else {
             showMessage({ title: "ユーザーが見つかりません", status: "error" });
+            setLoading(false);
           }
         })
-        .catch(() =>
-          showMessage({ title: "ログインできません。", status: "error" })
-        )
-        .finally(() => setLoading(false));
+        .catch(() => {
+          showMessage({ title: "ログインできません。", status: "error" });
+          setLoading(false);
+        });
     },
-    [history, showMessage]
+    [history, showMessage, setLoginUser]
   );
   return { login, loading };
 };
+
+// .finally(() => setLoading(false));はsetLoginUserでerrorが出る時はerrorでsetLoadiingをfaluseにするような設定を実施する必要がある。
